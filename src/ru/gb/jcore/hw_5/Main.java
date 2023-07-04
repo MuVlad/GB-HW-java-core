e2b3bca65a9780fc06cf85471714433012874ee7
Vlad Muslimov <muslimov.vlad@mail.ru>
2023-07-04 23:23:54 +0500

    Home work 4. Added task 2

--- src/ru/gb/jcore/hw_5/Main.java
+++ src/ru/gb/jcore/hw_5/Main.java
@@ -1,11 +1,24 @@
 package ru.gb.jcore.hw_5;
 
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Backup.createBackup("C:/Users/musli/IdeaProjects/GB-HW-java-core/src/ru/gb/jcore/hw_4", "./backup");
+            Backup.createBackup("src/ru/gb/jcore/hw_4", "./backup");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        writeToFile(new byte[]{0,1,1,0,2,1,2,0,3});
+
+    }
+
+    private static void writeToFile(byte[] arr) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("src/ru/gb/jcore/hw_5/someFile.txt")))) {
+            for (byte b : arr) {
+                writer.write(String.valueOf(b));
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
